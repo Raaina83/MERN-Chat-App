b8ac248a78caef13c3589e137cfd69c065912149
Raaina <raainawhalla83@gmail.com>
2025-02-05 09:19:09 +0530

    "npm package error"

--- frontend/vite-project/src/socket.jsx
+++ frontend/vite-project/src/socket.jsx
@@ -1,35 +1,13 @@
-import { useEffect } from 'react'
 import { createContext, useContext, useMemo } from 'react'
 import io from 'socket.io-client'
 
 
 const SocketContext = createContext()
 
-const getSocket = () => useContext(SocketContext)
-// const getSocket = () => {
-//     const socket = useContext(SocketContext);
-//     if (!socket) {
-//       throw new Error('getSocket must be used within a SocketProvider');
-//     }
-//     return socket;
-//   };
+const getSocket = () => useContext(SocketContext);
   
 
 const SocketProvider = ({children}) => {
-    // const socket = useMemo(() => 
-    //     io('http://localhost:5000', {
-    //     withCredentials: true,
-    //     // auth: { token: localStorage.getItem("token")}
-    // }), [])
-
-    // useEffect(() => {
-    //     return () => {
-    //       if (socket) {
-    //         // Remove event listeners instead of disconnecting the socket
-    //         socket.removeAllListeners(); // Clean up listeners
-    //       }
-    //     };
-    //   }, [socket]);
     const socket = useMemo(() => {
         const instance = io('http://localhost:5000', {
           withCredentials: true,
