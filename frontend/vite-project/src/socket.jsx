508cbd09505bf9077f5557724873d7b8f885b486
Raaina <raainawhalla83@gmail.com>
2025-02-05 18:54:51 +0530

    "socket url fixed"

--- frontend/vite-project/src/socket.jsx
+++ frontend/vite-project/src/socket.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useMemo } from 'react'
 import io from 'socket.io-client'
+import { server } from "./constants/config"
 
 
 const SocketContext = createContext()
@@ -9,7 +10,7 @@ const getSocket = () => useContext(SocketContext);
 
 const SocketProvider = ({children}) => {
     const socket = useMemo(() => {
-        const instance = io('http://localhost:5000', {
+        const instance = io(`${server}`, {
           withCredentials: true,
         });
         instance.on('connect', () => {
